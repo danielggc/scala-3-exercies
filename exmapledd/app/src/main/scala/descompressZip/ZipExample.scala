6130ea63af47822b361365528593a066d239e29e
dgc7 <dgc7@dgc7.dgc7>
2024-01-19 08:40:41 -0500

    add objec option

--- exmapledd/app/src/main/scala/descompressZip/ZipExample.scala
+++ exmapledd/app/src/main/scala/descompressZip/ZipExample.scala
@@ -13,13 +13,29 @@ import scala.util.{Failure, Success}
 
 @js.native
 trait JSZip extends js.Object {
-  def loadAsync(data: Blob): js.Promise[Unit] = js.native
+  def loadAsync(data:js.Any,
+                options: JSZipLoadOptions ): js.Promise[Unit] = js.native
   def file(name: String): JSZipObject = js.native
 }
 
 @js.native
 trait JSZipObject extends js.Object {
   def async(dataType: String): js.Promise[js.Dynamic] = js.native
+}
+
+@js.native
+trait JSZipLoadOptions extends js.Object {
+  var option1: js.UndefOr[String]
+  var option2: js.UndefOr[Int]
+  // Puedes agregar más opciones según la documentación de JSZip
+}
+object JSZipLoadOptions {
+  @inline
+  def apply(option1: String , option2:Int ): JSZipLoadOptions =
+    js.Dynamic.literal(
+      option1 = option1,
+      option2 = option2
+    ).asInstanceOf[JSZipLoadOptions]
 }
 
 
@@ -29,11 +45,12 @@ object ZipExample {
     val jsZip =  js.Object().asInstanceOf[JSZip]
     val fileReader =  FileReader()
     val promise = Promise[String]()
+    val options = JSZipLoadOptions(option1 = "value", option2 = 42)
 
     fileReader.onload = { ( event: Event ) =>
       val arrayBuffer = fileReader.result.asInstanceOf[ArrayBuffer]
 
-      jsZip.loadAsync( Blob(js.Array(arrayBuffer))).toFuture.flatMap { _ =>
+      jsZip.loadAsync( Blob(js.Array(arrayBuffer)) , options ).toFuture.flatMap { _ =>
         val contentPromise = jsZip.file("data1.txt").async("string").toFuture
         contentPromise.map(_.toString)
       }.onComplete {
